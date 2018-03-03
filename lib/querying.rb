88353a3da32193d30014cbcf23db445ea259c970
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-03 05:59:33 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -10,15 +10,21 @@ from projects
 order by length(projects.title) desc LIMIT 1;
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT"
+  "SELECT serires.name, COUNT(series.name) FROM series
+  INNER JOIN series ON books.series_id=series.id
+  ORDER BY COUNT(series.name;"
 end
 
-def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+def select_name_and_series_subgenres_of_authors #what is it asking???
+  "SELECT authors.name, sub_genres.name FROM authors
+  INNER JOIN authors ON series.author_id=authors.id
+  INNER JOIN sub_genres ON sub_genres.id=serires.sub_genre_id;"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title, COUNT FROM series
+  INNER JOIN series ON characters.series_id=series.id
+   "
 end
 
 def select_character_names_and_number_of_books_they_are_in
--- lib/schema.sql
+++ lib/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE series (id INTEGER PRIMARY KEY, name TEXT);
+CREATE TABLE series (id INTEGER PRIMARY KEY, title TEXT, author_id INTEGER, sub_genre_id INTEGER);
 CREATE TABLE sub_genres (id INTEGER PRIMARY KEY, name TEXT);
 CREATE TABLE authors (id INTEGER PRIMARY KEY, name TEXT);
 CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT, year INTEGER, series_id INTEGER);
