64b81c29e63bd2bcae9217eb26b4b05d17fa5c72
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-03 06:05:44 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -3,11 +3,9 @@ def select_books_titles_and_years_in_first_series_order_by_year
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT characters.name, characters.motto FROM characters ORDER BY length(characters.motto) DESC LIMIT 1"
+  "SELECT characters.name, characters.motto FROM characters ORDER BY length(characters.motto) DESC LIMIT 1;"
 end
-select projects.title
-from projects
-order by length(projects.title) desc LIMIT 1;
+
 
 def select_value_and_count_of_most_prolific_species
   "SELECT serires.name, COUNT(series.name) FROM series
@@ -22,11 +20,13 @@ def select_name_and_series_subgenres_of_authors #what is it asking???
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title, COUNT FROM series
+  "SELECT series.title, COUNT(characters.species) FROM series
   INNER JOIN series ON characters.series_id=series.id
-   "
+  GROUP BY characters.species DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(books.title) FROM character
+  INNER JOIN characters ON books.series_id=characters.series_id;
+  "
 end
