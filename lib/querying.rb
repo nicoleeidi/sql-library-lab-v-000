81cfd4292cc9d0eae099e828c23625fc6c77e72e
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-04 23:22:28 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -9,7 +9,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters.species, COUNT(series.title) FROM series
+  "SELECT characters.species, COUNT(characters.species) FROM characters
   INNER JOIN books ON books.series_id=series.id
   ORDER BY COUNT(series.title);"
 end
