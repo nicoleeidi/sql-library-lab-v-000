7a8d56df67b1b06a71f0190c9aca7be4ff003b23
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-04 15:24:29 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,6 +1,6 @@
 def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT books.title, books.year FROM books
-  ORDER BY books.year WHERE series_id=1;"
+   WHERE series_id=1 ORDER BY books.year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
@@ -10,25 +10,25 @@ end
 
 def select_value_and_count_of_most_prolific_species
   "SELECT series.title, COUNT(series.title) FROM series
-  INNER JOIN series ON books.series_id=series.id
+  INNER JOIN books ON books.series_id=series.id
   ORDER BY COUNT(series.title);"
 end
 
 def select_name_and_series_subgenres_of_authors #what is it asking???
   "SELECT authors.name, subgenres.name FROM authors
-  INNER JOIN authors ON series.author_id=authors.id
-  INNER JOIN subgenres ON subgenres.id=serires.sub_genre_id;"
+  INNER JOIN series ON series.author_id=authors.id
+  INNER JOIN subgenres ON subgenres.id=serires.subgenre_id;"
 end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title, COUNT(characters.species) FROM series
-  INNER JOIN series ON characters.series_id=series.id
-  GROUP BY characters.species ORDER BY COUNT(characters.speices) DESC LIMIT 1;"
+  INNER JOIN characters ON characters.series_id=series.id
+  GROUP BY characters.species ORDER BY COUNT(characters.species) DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(books.title) FROM characters
-  INNER JOIN characters ON books.series_id=characters.series_id
-  ORDER BY COUNT(books.title) DESC;
+  "SELECT characters.name, COUNT(character_books.id) FROM characters
+ INNER JOIN character_books ON character_books.character_id=characters.id
+ GROUP BY characters.name ORDER BY COUNT(character_books.id) DESC;
   " #would books.title be the right thing to put into count? just want to count books
 end
